69f0bd28ee8373a7459852c4e21168fbf033ec6d
avinash <avinashkr8920@gmail.com>
2024-10-13 11:25:03 +0530

    Initial commit

---
+++ kafka-producer/src/test/java/com/learningwithak/kafka/procuder/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningwithak.kafka.procuder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
